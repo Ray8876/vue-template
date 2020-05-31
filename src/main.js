58dc952d2a43a8e748ebc981c9e807b40b350782
songyuli <123456>
2020-05-31 20:46:48 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
 // import './assets/css/theme-green/index.css'; // 浅绿色主题
 import './assets/css/icon.css';
+import { uuid , copyObj , copyArray} from './util/common'
+
+Vue.prototype.$uuid = uuid;
+Vue.prototype.$copyobj = copyObj;
+Vue.prototype.$copyArray = copyArray;
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI, {
